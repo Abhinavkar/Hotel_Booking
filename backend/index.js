4c0e0f09bc91def9faa37d1748c236adc20469ea
Abhinav kar <75711413+Abhinavkar@users.noreply.github.com>
2023-01-28 09:14:40 +0000

    Added Routes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,36 @@
 import  express  from "express";
-const app =express()
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoutes from "./routes/auth.js"
 
-app.listen(8000,()=>{console.log("Connected to Backend")} )
+
+
+
+const app =express()
+dotenv.config();
+
+const connect = async()=>{
+	try{
+		await mongoose.connect(process.env.DbUrl);
+		console.log("Connected to Database")
+	} 
+	catch (error) 
+	{
+		throw error ;
+	}
+	mongoose.connection.on("disconnected",()=>{
+	console.log("MongoDb Disconnected ")
+	})
+	mongoose.connection.on("Connected ",()=>{
+	console.log("MongoDb Connnected ")
+	})
+};
+
+//middle ware 	
+app.use("/auth",authRoutes);
+
+
+app.listen(8000,()=>{
+	connect()
+	console.log("Connected to Backend");
+});	
