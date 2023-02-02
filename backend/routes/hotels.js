c281c345a9ed7b36d1a5f4112467c842a9defbc9
ABHINAV <karabhinav24@gmail.com>
2023-02-02 19:53:39 +0530

    Added Working api

--- backend/routes/hotels.js
+++ backend/routes/hotels.js
@@ -10,5 +10,46 @@ router.post("/",async(req,res)=>{
 	catch(err){
 		res.status(500).json(err)}
 	
-})
+});
+
+router.put("/:id", async (req,res) => {
+	
+	try{
+		const updatedHotel = await Hotel.findByIdAndUpdate( req.params.id ,{$set : req.body},{new:true})
+		res.status(200).json(updatedHotel);
+	}
+	catch(err){
+		res.status(500).json(err);}
+	
+}); 
+router.delete("/:id", async (req,res) => {
+	
+	try{
+		await Hotel.findByIdAndUpdate( req.params.id );
+		res.status(200).json("Deleted Hotel");
+	}
+	catch(err){
+		res.status(500).json(err);}
+	
+}); 
+router.get("/:id", async (req,res) => {
+	
+	try{
+		const hotel= await Hotel.findById( req.params.id );
+		res.status(200).json(hotel);
+	}
+	catch(err){
+		res.status(500).json(err);}
+	
+}); 
+router.get("/", async (req,res) => {
+	
+	try{
+		const hotels= await Hotel.find(  );
+		res.status(200).json(hotels);
+	}
+	catch(err){
+		res.status(500).json(err);}
+	
+}); 
 export default router ;
