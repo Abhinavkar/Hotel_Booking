034dba58a32464a366bd424810f14a67198cbfae
Abhinavkar <karabhinav24@gmail.com>
2023-04-12 06:49:17 +0530

    Added Cookie-parser

--- backend/controllers/authcontroller.js
+++ backend/controllers/authcontroller.js
@@ -18,7 +18,6 @@ export const register =async (req,res,next)=>{
 	catch(err){
 		next(err)
 	}
-
 }
 
 export const login  =async (req,res,next)=>{
@@ -26,15 +25,17 @@ export const login  =async (req,res,next)=>{
 		const user =await  User.findOne({username:req.body.username})
 		if(!user) 
 			return next(createError(404,"User not Found "))
+
 		const isPasswordCorrect = await bcrypt.compare(req.body.password,user.password)
+		
 		if(!isPasswordCorrect)
 			return next(createError(400,"password is  incorrect"));
-		
-		const token = jwt.sign({id:user._id, isAdmin:user.isAdmin})
 
+		const token = jwt.sign({id:user._id, isAdmin:user.isAdmin},process.env.Jwt);
 		const {password ,isAdmin, ...otherDetails}=user._doc
-		
-		res.status(200).json({...otherDetails});
+		res.cookie("access_token", token,{
+			httpOnly:true,
+		}).status(200).json({...otherDetails});
 	}
 	catch(err){
 		next(err)
